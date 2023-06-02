1f442936aadfb37c37a4708254a5211d373def7e
Swiggui <figueredojuan2010@gmail.com>
2023-06-02 17:12:09 -0500

    Estructuración página del equipo

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,18 +1,31 @@
 import TeamData from "../Data/TeamData";
+import '../Styles/memberStyles.css';
 
 const Member = () => {
     return(
-        <div className="member">
-            <div>
-        <iframe scrolling="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/269248772&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" width="100%" height={300} frameBorder="no">
-        </iframe>
-        <div style={{fontSize: '10px', color: '#cccccc', lineBreak: 'anywhere', wordBreak: 'normal', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight: 100}}>
-          <a href="https://soundcloud.com/teenagewrist" title="Teenage Wrist" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>Teenage Wrist</a> · 
-          <a href="https://soundcloud.com/teenagewrist/stoned-alone" title="Stoned, Alone" target="_blank" style={{color: '#cccccc', textDecoration: 'none'}}>Stoned, Alone</a>
+        <div id="members">
+            {TeamData.map((member, index) => (
+                <div 
+                    className="member"
+                    key={index}>
+                    <img src={member.PhotoPath}/>
+                    <article className="member-info">
+                        <h2>{member.Name}</h2>
+                        <h3>{member.Role}</h3>
+                        <p>{member.MiniBio}</p>
+                    </article>
+                    <div>
+                        <iframe
+                            className="media"
+                            allow="autoplay"
+                            src={member.MainProject}
+                            width="75%"
+                            height={500}/>
+                    </div>
+                </div>
+            ))}
         </div>
-      </div>
-        </div>
-    )
+    );
 }
 
 export default Member;
