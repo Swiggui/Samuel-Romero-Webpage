b15eba09191968c4d971333bc801798e8a848174
Swiggui <figueredojuan2010@gmail.com>
2023-06-23 18:27:47 -0500

    Removiendo componente creado de forma inecesaria

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -1,10 +1,47 @@
-import Member from "../components/Member";
+import TeamData from "../Data/TeamData";
+import Social from "../components/Social";
+import '../Styles/memberStyles.css';
 
 const Credits = () => {
     return(
         <main>
             <h1>Meet our team!</h1>
-            <Member/>
+            
+            <div id="members">
+    {TeamData.map((member, index) => (
+        <div 
+            className="member"
+            key={index}>
+            <img className="member-photo " src={require(`../Media/Images/TeamImages/${member.PhotoPath}`)} alt={"A smile of " + member.Name}/>
+            <article className="member-info">
+                <h2 className="fw-bold">{member.Name}</h2>
+                <h3 className="fw-light">{member.Role}</h3>
+                <p>{member.MiniBio}</p>
+            </article>
+            <div>
+                <iframe
+                    className="member-project"
+                    allow="autoplay"
+                    src={member.MainProject}
+                    width="75%"
+                    height={500}
+                    title={"Featured project of " + member.Name}/>
+            </div>
+            <div className="member-social-networks">
+            {
+                member.SocialNetworks.map((social, index) => (
+
+                            <Social
+                                name = {social.name}
+                                link = {social.link}
+                            />
+                    )
+                )
+            }
+            </div>
+        </div>
+    ))}
+</div>
         </main>
     );
 }
