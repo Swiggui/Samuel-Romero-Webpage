8851fc9ee1009eba4b6f2611bc6ba2a22b898240
Swiggui <figueredojuan2010@gmail.com>
2023-06-23 21:43:34 -0500

    Avances al alinear los items

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -3,45 +3,46 @@ import Social from "../components/Social";
 import '../Styles/memberStyles.css';
 
 const Credits = () => {
-    return(
+    return (
         <main>
             <h1>Meet our team!</h1>
-            
-            <div id="members">
-    {TeamData.map((member, index) => (
-        <div 
-            className="member"
-            key={index}>
-            <img className="member-photo " src={require(`../Media/Images/TeamImages/${member.PhotoPath}`)} alt={"A smile of " + member.Name}/>
-            <article className="member-info">
-                <h2 className="fw-bold">{member.Name}</h2>
-                <h3 className="fw-light">{member.Role}</h3>
-                <p>{member.MiniBio}</p>
-            </article>
-            <div>
-                <iframe
-                    className="member-project"
-                    allow="autoplay"
-                    src={member.MainProject}
-                    width="75%"
-                    height={500}
-                    title={"Featured project of " + member.Name}/>
-            </div>
-            <div className="member-social-networks">
-            {
-                member.SocialNetworks.map((social, index) => (
 
-                            <Social
-                                name = {social.name}
-                                link = {social.link}
-                            />
-                    )
-                )
-            }
+            <div id="members">
+                {TeamData.map((member, index) => (
+                    <div
+                        className="member"
+                        key={index}>
+
+                        <img className="member-photo " src={require(`../Media/Images/TeamImages/${member.PhotoPath}`)} alt={"A smile of " + member.Name} />
+                        <article className="member-info">
+                            <h2 className="fw-bold">{member.Name}</h2>
+                            <h3 className="fw-light">{member.Role}</h3>
+                            <p>{member.MiniBio}</p>
+                        </article>
+                        <div>
+                            <iframe
+                                className="member-project"
+                                allow="autoplay"
+                                src={member.MainProject}
+                                width="75%"
+                                height={500}
+                                title={"Featured project of " + member.Name} />
+                        </div>
+                        <div className="member-social-networks">
+                            {
+                                member.SocialNetworks.map((social, index) => (
+
+                                    <Social
+                                        name={social.name}
+                                        link={social.link}
+                                    />
+                                )
+                                )
+                            }
+                        </div>
+                    </div>
+                ))}
             </div>
-        </div>
-    ))}
-</div>
         </main>
     );
 }
