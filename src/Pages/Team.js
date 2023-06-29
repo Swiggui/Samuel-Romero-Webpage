5dffd7393c3d7cbcccd1683dfdc828c1ed7c55a9
Swiggui <figueredojuan2020@outlook.es>
2023-06-28 19:12:34 -0500

    Archivo preparado para modificar vista en desktop

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -13,19 +13,22 @@ const Credits = () => {
                         className="member"
                         key={index}>
 
-                        <img className="member-photo " src={require(`../Media/Images/TeamImages/${member.PhotoPath}`)} alt={"A smile of " + member.Name} />
-                        <article className="member-info">
-                            <h2 className="fw-bold">{member.Name}</h2>
-                            <h3 className="fw-light">{member.Role}</h3>
-                            <p>{member.MiniBio}</p>
-                        </article>
+                        <div
+                            className="member-data">
+                            <img className="member-photo " src={require(`../Media/Images/TeamImages/${member.PhotoPath}`)} alt={"A smile of " + member.Name} />
+                            <article className="member-info">
+                                <h2 className="fw-bold">{member.Name}</h2>
+                                <h3 className="fw-light">{member.Role}</h3>
+                                <p>{member.MiniBio}</p>
+                            </article>
+                        </div>
                         <div>
                             <iframe
                                 className="member-project"
                                 allow="autoplay"
                                 src={member.MainProject}
                                 width="75%"
-                                
+
                                 title={"Featured project of " + member.Name} />
                         </div>
                         <div className="member-social-networks">
